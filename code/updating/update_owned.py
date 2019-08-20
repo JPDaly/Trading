3035a912eed0e7eff281425c4f31fca573d80797
JPDaly <jordanpatdaly@gmail.com>
2019-08-21 01:29:03 +1000

    More planning for pattern class

--- code/updating/update_owned.py
+++
@@ -1,2 +0,0 @@
-# Like the name suggests this will allow me to input what I have bought (how many and for how much)
-# and remove those that I have sold
--- code/voting/patterns.py
+++ code/voting/patterns.py
@@ -18,12 +18,43 @@ class Pattern:
 		for max_period in range(SHORTEST_PATTERN,LONGEST_PATTERN+1):
 			sub_prices = prices[-max_period:]
 			n_sub_prices = len(sub_prices)
-			prices_sorted = sorted(zip(sub_prices,range(n_sub_prices), reverse=True)
+			prices_sorted = sorted(zip(sub_prices,range(n_sub_prices)), reverse=True)
 			self.means.append(sum(sub_prices)/n_sub_prices)
 			self.max_prices.append(prices_sorted[:3])
 			self.min_prices.append(prices_sorted[-3:])
 
 			if 2*max_period >= self.n_prices:
-				tail_means.append(sum(prices[:-max_period])/len(prices[:-max_period]))
+				self.tail_means.append(sum(prices[:-max_period])/len(prices[:-max_period]))
 			else:
-				tail_means.append(sum(prices[:-max_period])/len(prices[-2*max_period:-max_period]))
+				self.tail_means.append(sum(prices[:-max_period])/len(prices[-2*max_period:-max_period]))
+
+
+	def res_triangle(self):
+		# return -1 to 1 depending on whether the tail is above or below
+		pass
+
+	def res_flag(self):
+		# same deal as triangle
+		pass
+
+	def res_head_and_shoulders(self):
+		# upside down return 1 
+		# normal return -1
+		pass
+
+	def res_double_top(self):
+		# same deal as head and shoulders
+		pass
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+	pattern = Pattern([26,10,19,28,10,14,15,30,9,16,7,16,8,14,20,30,15,27,25,5,26,11,12,2,15,12,16,3,28,9])
+	print(pattern.means)
