c86caf467df4a0f18662f2719360aefdd1d40dab
JPDaly <jordanpatdaly@gmail.com>
2019-12-17 01:08:45 +1100

    Still trying to get it working again

--- code/scraping/companies.py
+++ code/scraping/companies.py
@@ -15,63 +15,68 @@ ZIP_FILE_LOCATION = "../../database/companies/temp.zip"
 
 
 def get_asx_companies():
-	all_companies = download_all_companies()
-	useful = download_useful_companies()
-	rows_before = all_companies.shape[0]
+    all_companies = download_all_companies()
+    useful = download_useful_companies()
+    rows_before = all_companies.shape[0]
 
-	companies_to_drop = []
-	for i,company in all_companies.iterrows():
-		if company['ASX code'] not in useful[0].values:
-			companies_to_drop.append(i)
+    companies_to_drop = []
+    for i,company in all_companies.iterrows():
+        if company['ASX code'] not in useful[0].values:
+            companies_to_drop.append(i)
 
-	all_companies.drop(all_companies.index[companies_to_drop], inplace=True)
-	all_companies.to_csv(FILE_LOCATION+FILE_NAME,index=False)
+    all_companies.drop(all_companies.index[companies_to_drop], inplace=True)
+    all_companies.to_csv(FILE_LOCATION+FILE_NAME,index=False)
 
-	print("Removed {0} companies".format(rows_before-all_companies.shape[0]))
+    print("Removed {0} companies".format(rows_before-all_companies.shape[0]))
 
 
 
 #This is no longer working because you require a header when using urlretrieve which isn't defined. 
 #Go to https://stackoverflow.com/questions/9419162/download-returned-zip-file-from-url to see how to maybe do it with requests
 def download_useful_companies():
-	headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'}
-	#The above is required otherwise the website doesn't give you access. I found this by going to the network tab in the dev tools in chrome 
-	# 	and searched (not filtered) for user-agent
-	response = requests.get(USEFUL_URL,headers=headers)
-	if not response.ok:
-		raise ValueError("Unable to retrieve page.")
-	page = bs(response.text, 'html.parser')
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'}
+    #The above is required otherwise the website doesn't give you access. I found this by going to the network tab in the dev tools in chrome 
+    #     and searched (not filtered) for user-agent
+    response = requests.get(USEFUL_URL,headers=headers)
+    if not response.ok:
+        raise ValueError("Unable to retrieve page.")
+    page = bs(response.text, 'html.parser')
 
-	for a in page.find_all('a', href=True):
-		if DOWNLOAD_URL_FORMAT in a['href']:
-			print(a['href'])
-			urlretrieve(a['href'],ZIP_FILE_LOCATION)
-			break
+    for a in page.find_all('a', href=True):
+        if DOWNLOAD_URL_FORMAT in a['href']:
+            r = requests.get(a['href'], headers=headers)
+            if not r.ok:
+                print("Error, couldn't retrieve zip file")
+                exit()
+            with open(ZIP_FILE_LOCATION,'wb') as f:
+                f.write(r.content)
+            #urlretrieve(a['href'],ZIP_FILE_LOCATION)
+            break
 
-	archive = zipfile.ZipFile(ZIP_FILE_LOCATION, 'r')
-	file_content = archive.open(archive.namelist()[0])
+    archive = zipfile.ZipFile(ZIP_FILE_LOCATION, 'r')
+    file_content = archive.open(archive.namelist()[0])
 
-	return pd.read_csv(file_content, header=None, usecols=[0])
-	
+    return pd.read_csv(file_content, header=None, usecols=[0])
+    
 
 
 def download_all_companies():
-	if not os.path.exists(FILE_LOCATION):
-		raise ValueError("Error. Couldn't find directory.")
+    if not os.path.exists(FILE_LOCATION):
+        raise ValueError("Error. Couldn't find directory.")
 
-	if os.path.isfile(FILE_LOCATION+FILE_NAME):
-		os.remove(FILE_LOCATION+FILE_NAME)
+    if os.path.isfile(FILE_LOCATION+FILE_NAME):
+        os.remove(FILE_LOCATION+FILE_NAME)
 
-	urlretrieve(COMPANIES_LIST,FILE_LOCATION + FILE_NAME)
+    urlretrieve(COMPANIES_LIST,FILE_LOCATION + FILE_NAME)
 
-	# Just to remove the extra space in the 
-	df = pd.read_csv(FILE_LOCATION + FILE_NAME,skiprows=2)
-	df.to_csv(FILE_LOCATION+FILE_NAME,index=False)
+    # Just to remove the extra space in the 
+    df = pd.read_csv(FILE_LOCATION + FILE_NAME,skiprows=2)
+    df.to_csv(FILE_LOCATION+FILE_NAME,index=False)
 
-	# may not need this return statement if I end up only using this function
-	return df
+    # may not need this return statement if I end up only using this function
+    return df
 
 
 if __name__ == "__main__":
-	# download_all_companies()
-	get_asx_companies()
+    # download_all_companies()
+    get_asx_companies()
--- code/scraping/statistics.py
+++ code/scraping/statistics.py
@@ -5,6 +5,8 @@ from bs4 import BeautifulSoup as bs
 from Yahoo import YahooStats
 import re
 import datetime
+import keyboard
+import time
 
 
 COMPANIES_LIST_LOCATION = "../../database/companies/asx.csv"
@@ -15,45 +17,70 @@ LAST_UPDATED = "../../database/historical/asx/stats/last_updated/{0}.txt"
 
 
 def get_statistics(date):
-	yahoo = YahooStats()
-	columns = ['asx code', 'Date', 'sector'] + yahoo.ROW_TITLES
+    yahoo = YahooStats()
+    columns = ['asx code', 'Date', 'sector'] + yahoo.ROW_TITLES
+    pause = False
+    companies = pd.read_csv(COMPANIES_LIST_LOCATION)
+    n_rows = companies.shape[0]
+    
+    statistics_df = pd.DataFrame(columns=columns)
+    print("Hold 'p' to pause and 'r' to resume")
+    for i,company in companies.iterrows():
+        while(True):
+            if pause or keyboard.is_pressed('p'):
+                pause = True
+            if not pause or keyboard.is_pressed('r'):
+                pause = False
+                break
+            time.sleep(2)
+            
+        print("Scraping stock " + str(i+1) + "/" + str(n_rows), end="\r")
+        
+        yahoo.set_stock(company['ASX code'])
+        statistics_df = statistics_df.append(pd.DataFrame([[yahoo.stock,date,company['GICS industry group']] + yahoo.get_stats()],columns=columns),ignore_index=True)
 
-	companies = pd.read_csv(COMPANIES_LIST_LOCATION)
-	n_rows = companies.shape[0]
-	
-	statistics_df = pd.DataFrame(columns=columns)
+    if os.path.isfile(STATISTICS_LOCATION):
+        os.remove(STATISTICS_LOCATION)
 
-	for i,company in companies.iterrows():
-		print("Scraping stock " + str(i+1) + "/" + str(n_rows), end="\r")
-		yahoo.set_stock(company['ASX code'])
-		statistics_df = statistics_df.append(pd.DataFrame([[yahoo.stock,date,company['GICS industry group']] + yahoo.get_stats()],columns=columns),ignore_index=True)
-
-	if os.path.isfile(STATISTICS_LOCATION):
-		os.remove(STATISTICS_LOCATION)
-
-	statistics_df.drop_duplicates().to_csv(STATISTICS_LOCATION,index=False)
-
+    statistics_df.drop_duplicates().to_csv(STATISTICS_LOCATION,index=False)
 
 
 def append_stats(date):
-	df = pd.read_csv(STATISTICS_LOCATION)
-	columns = df.columns
-	# there's no check for the last time this was updated. Instead it's possible to use drop_duplicates() as shown in https://jamesrledoux.com/code/drop_duplicates
-	# to avoid dealing with them later on
-	for i,company in df.iterrows():
-		if os.path.isfile(STATS_DATABASE_LOC.format(company['asx code'])):
-			temp = pd.read_csv(STATS_DATABASE_LOC.format(company['asx code']))
-			temp = temp.append(company,ignore_index=True)
-			temp.to_csv(STATS_DATABASE_LOC.format(company['asx code']),index=False)
-		else:
-			temp = pd.DataFrame([company.values],columns=columns)
-			temp.to_csv(STATS_DATABASE_LOC.format(company['asx code']),index=False)
+    df = pd.read_csv(STATISTICS_LOCATION)
+    columns = df.columns
+    # there's no check for the last time this was updated. Instead it's possible to use drop_duplicates() as shown in https://jamesrledoux.com/code/drop_duplicates
+    # to avoid dealing with them later on
+    for i,company in df.iterrows():
+        print(str(i+1),end="\r")
+        if(i==1010):
+            print(company)
+        if os.path.isfile(STATS_DATABASE_LOC.format(company['asx code'])):
+            if(i==1010):
+                print("Here1")
+            temp = pd.read_csv(STATS_DATABASE_LOC.format(company['asx code']))
+            if(i==1010):
+                print("Here2")
+            temp = temp.append(company,ignore_index=True)
+            if(i==1010):
+                print("Here3")
+            temp.to_csv(STATS_DATABASE_LOC.format(company['asx code']),index=False)
+        else:
+            if(i==1010):
+                print("Here4")
+            temp = pd.DataFrame([company.values],columns=columns)
+            if(i==1010):
+                print("Here5")
+                print(temp)
+                print(STATS_DATABASE_LOC.format(company['asx code']))
+            temp.to_csv(STATS_DATABASE_LOC.format(company['asx code']),index=False)
 
-		#currently only doing this in case I want to use it in the future for something 
-		with open(LAST_UPDATED.format(company['asx code']),"w") as f:
-			f.write(date)
+        #currently only doing this in case I want to use it in the future for something 
+        if(i==1010):
+            print("Here6")
+        with open(LAST_UPDATED.format(company['asx code']),"w") as f:
+            f.write(date)
 
 
 if __name__ == "__main__":
-	get_statistics(datetime.date.today().strftime("%Y-%m-%d"))
-	append_stats(datetime.date.today().strftime("%Y-%m-%d"))
+    #get_statistics(datetime.date.today().strftime("%Y-%m-%d"))
+    append_stats(datetime.date.today().strftime("%Y-%m-%d"))
